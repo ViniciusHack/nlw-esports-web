9f5dfe3d03250bd78fce4262e992faef118fd6fa
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-09-18 16:54:53 -0300

    feat: radix modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import './styles/main.css';
 
+import * as Dialog from '@radix-ui/react-dialog';
+import { useEffect, useState } from 'react';
 import logoImg from './assets/logo-nlw-esports.svg';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { GameCard } from './components/GameCard';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 export function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8888/games")
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+
   return (
     <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
       <img src={logoImg} alt="" />
@@ -13,14 +32,29 @@ export function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameCard
-          title='League of Legends'
-          bannerUrl='https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg'
-          adsCount={1}
-        />
+        {games.map(game => 
+          <GameCard
+            key={game.id}
+            title={game.title}
+            bannerUrl={game.bannerUrl}
+            adsCount={game._count.ads}
+          />
+        )}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed"/>
+
+          <Dialog.Content className="fixed shadow-lg shadow-black/25 bg-[#2A2634] rounded-lg w-[480px] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
+
+            <form></form>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,7 +7,7 @@ interface GameCardProps {
 export function GameCard({ adsCount, bannerUrl, title }: GameCardProps) {
   return (
     <a href="" className="relative rounded-lg overflow-hidden">
-      <img src={bannerUrl} alt="" />
+      <img className="w-full" src={bannerUrl} alt={title} />
 
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{title}</strong>
